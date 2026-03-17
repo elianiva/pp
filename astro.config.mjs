1262a7929501be82ae160f02bf4888d190201c4e
elianiva <git@elianiva.my.id>
2026-03-17 17:45:15 +0700

    chore: add astro db, tailwindcss, solidjs, and shiki dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import db from "@astrojs/db";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [db(), solidJs()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
